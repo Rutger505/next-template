ad6a4af57b3879d1b5e182856e37c2e7605ab63d
Rutger <rutger.pronk11@gmail.com>
2025-01-28 00:01:20 +0100

    Try out typescript

--- .github/scripts/deployment/config.ts
+++ .github/scripts/deployment/config.ts
@@ -1,16 +1,14 @@
-// @ts-check
-
+import * as core from "@actions/core";
+import { Context } from "@actions/github/lib/context";
 import fs from "fs";
 import path from "path";
 
-/**
- * Reads the application.env file and returns its contents as an object
- * @returns {Record<string, string>}
- */
-function readEnvFile() {
+type Core = typeof core;
+
+function readEnvFile(): Record<string, string> {
   const envPath = path.join(process.cwd(), "application.env");
   const envContent = fs.readFileSync(envPath, "utf8");
-  const envVars = {};
+  const envVars: Record<string, string> = {};
 
   envContent.split("\n").forEach((line) => {
     const [key, value] = line.split("=").map((part) => part.trim());
@@ -22,27 +20,31 @@ function readEnvFile() {
   return envVars;
 }
 
-/** @param {import('@actions/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-export default function generateConfig({ context, core }) {
+export default async function generateConfig({
+  context,
+  core,
+}: {
+  context: Context;
+  core: Core;
+}) {
   try {
     const isTag = context.ref.startsWith("refs/tags/");
     const tag = isTag ? context.ref.replace("refs/tags/", "") : undefined;
 
     const config = {
       is_production: isTag ? "true" : "false",
-      tag: isTag ? tag : context.sha,
+      tag: isTag ? tag! : context.sha,
       environment: isTag
         ? "production"
         : `pr-${context.payload.pull_request?.number}`,
       hostname: isTag
-        ? process.env.BASE_DOMAIN
+        ? process.env.BASE_DOMAIN!
         : `${context.sha}.${process.env.BASE_DOMAIN}`,
       certificate_issuer: isTag
         ? "letsencrypt-production"
         : "letsencrypt-staging",
     };
 
-    // Read application.env file
     const envVars = readEnvFile();
 
     const outputs = {
@@ -61,7 +63,9 @@ export default function generateConfig({ context, core }) {
 
     return outputs;
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
     throw error;
   }
 }
