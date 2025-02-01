0bee6d8e98aea9df9f4eaee0a93ab58d78006d87
Rutger <rutger.pronk11@gmail.com>
2025-02-02 00:31:10 +0100

    Make migrating with drizzle possible by just using lib/sqlite

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as schema from "./schema";
 
 /**
